a72fc05cefbec7748570cb4a544ce700bb9fbfee
Diego Cedillo <92384848+DiegoCedilloCastillo@users.noreply.github.com>
2024-12-15 19:53:36 -0600

    Add files via upload

---
+++ DCOMServidor/DCOMServidor/ISaludo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DCOMServidor
+{
+    [ComVisible(true)] // Hace que la interfaz sea visible para COM
+    [Guid("D1234567-89AB-4CDE-8F01-234567890ABC")] // GUID único para la interfaz
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)] // Permite acceso desde COM y .NET
+    public interface ISaludo
+    {
+        string Saludar(string nombre);
+    }
+}
---
+++ DCOMServidor/DCOMServidor/Saludo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace DCOMServidor
+{
+    [ComVisible(true)] // Hace visible la clase para COM
+    [Guid("F1234567-89AB-4CDE-8F01-234567890ABC")] // GUID único para la clase
+    [ClassInterface(ClassInterfaceType.None)] // Requiere que implemente la interfaz explícitamente
+    public class Saludo : ISaludo
+    {
+        public string Saludar(string nombre)
+        {
+            return $"Hola, {nombre}! Bienvenido al mundo de DCOM.";
+        }
+    }
+}
